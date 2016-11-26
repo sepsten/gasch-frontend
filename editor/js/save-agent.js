2b71755da5e8974f80f579b47c8f4e95523232ab
sepsten <anatole.divoux@gmail.com>
2016-11-26 17:43:24 +0100

    Improve the save agent slightly and prepare its next version

--- editor/js/save-agent.js
+++ editor/js/save-agent.js
@@ -1,14 +1,34 @@
+var api = require("./../../common/js/client");
+
 /**
- * Deals with saving or creating documents.
+ * Deals with saving or creating documents on the server.
  *
  * @class
- * @param {GaschAPI} lapi - An API client instance
  * @param {String} [id] - The current's document ID if it is known
+ * @param {Piotr.History} history - The current Piotr history instance
  */
 class SaveAgent {
-  constructor(api, id)	{
+  constructor(id)	{
+    /**
+     * A reference to the API client.
+     *
+     * @type {GaschAPI}
+     */
     this.api = api;
+
+    /**
+     * The current document's ID.
+     *
+     * @type {Number}
+     */
     this.id = id;
+
+    /**
+     * A reference to the current history instance.
+     *
+     * @type {Piotr.History}
+     */
+    this.history = history;
   }
 
   /**
@@ -20,16 +40,34 @@ class SaveAgent {
   save(data) {
     var p, self = this;
 
-    // 1. If we have a new document...
-    if(!this.id)
-      p = this.api.post("/documents", data).then(function(res){
-        self.id = res.body.id;
-      });
-    else
-      p = this.api.put("/documents/" + this.id, data);
+    var url = "/documents";
+    var method = "post";
+    // If the document already exists, add its ID to the URL
+    if(this.id) {
+      url += "/" + this.id;
+      method = "put";
+    }
+
+    p = this.api[method](url, data).then(function(res) {
+      // Update the stored ID
+      if(res.body) self.id = res.body.id;
+    });
 
     return p;
   }
+
+  /**
+   * Returns true if there are changes to be saved.
+   *
+   * @returns {Boolean}
+   */
+  /*canSave() {
+    // Don't save is the document was not modified since opening.
+    if(this.lastChange === null) return false;
+
+    // Return: is history's cursor different from our last saved change ?
+    return this.lastChange !== this.history.stack[this.history.cursor];
+  }*/
 }
 
 module.exports = SaveAgent;
